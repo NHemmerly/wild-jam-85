a45209e82b34565a136bfd2e602c2e01e8efa95b
Neil Hemmerly <neilhemm0@gmail.com>
2025-09-14 01:46:44 -0400

    add moving camera to player node

--- game/player/player.gd
+++ game/player/player.gd
@@ -1,12 +1,31 @@
 class_name Player
 extends Node2D
 
+const CAM_SPEED: float = 25
+@export var cam: Camera2D
+var cam_direction: Vector2 = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	cam.position += CAM_SPEED * cam_direction
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("cam_up"):
+		cam_direction = Vector2.UP
+	if Input.is_action_pressed("cam_down"):
+		cam_direction = Vector2.DOWN
+	if Input.is_action_pressed("cam_left"):
+		cam_direction = Vector2.LEFT
+	if Input.is_action_pressed("cam_right"):
+		cam_direction = Vector2.RIGHT
+	if not (
+		Input.is_action_pressed("cam_up") or
+		Input.is_action_pressed("cam_down") or
+		Input.is_action_pressed("cam_left") or 
+		Input.is_action_pressed("cam_right")
+	):
+		cam_direction = Vector2.ZERO
